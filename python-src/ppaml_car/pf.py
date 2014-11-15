e48e647e4d5338429dedd509623eb7b618ced86a
Constantin Berzan <cberzan@gmail.com>
2014-11-14 16:56:09 -0800

    wip debugging the pf

--- python-src/ppaml_car/pf.py
+++ python-src/ppaml_car/pf.py
@@ -12,11 +12,13 @@ from ppaml_car.lasers import car_loc_to_laser_loc
 from ppaml_car.lasers import default_laser_angles
 from ppaml_car.lasers import default_laser_max_range
 from ppaml_car.lasers import plot_lasers
+from ppaml_car.lasers import plot_obstacles
 
 from numutil import logaddexp_many
 from numutil import norm_log_pdf_id_cov
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 import sys
 
 
@@ -125,25 +127,34 @@ class LocPF(PF):
         super(LocPF, self).__init__(num_particles)
         self.dataset = dataset
         self.last_control_ts = 0
-        self.dyn_noise_stdev = 0.003
-        self.obs_cov_scale = 0.1
+        self.dyn_noise_stdev = 0.03
+        self.obs_cov_scale = 30.0
         self.laser_angles = default_laser_angles()
         self.laser_max_range = default_laser_max_range()
 
         # Convert obstacles from list of (x, y) to array of (x, y, r).
-        obstacle_radius = 0.5
+        obstacle_radius = 0.35
         self.obstacles = []
         for x, y in dataset.ground_obstacles:
             self.obstacles.append((x, y, obstacle_radius))
         self.obstacles = np.array(self.obstacles)
 
-        # Plot ground-truth and dead-reckoning trajectories.
         self.fig1 = plt.figure()
         self.ax1 = self.fig1.add_subplot(111)
+
+        # Hack to display colorbar for angle.
+        a = np.array([[-np.pi, np.pi]])
+        x = plt.pcolor(a)
+        self.ax1.clear()
+        plt.colorbar(x)
+
+        # Plot ground-truth and dead-reckoning trajectories.
         gps_poses = get_ground_truth_poses(dataset)
         plot_traj(self.ax1, 'ground', *gps_poses)
         dr_poses = get_dead_reckoning_poses(dataset, dynamics)
         plot_traj(self.ax1, 'dead-reckoning', *dr_poses)
+        plot_obstacles(self.obstacles, self.ax1)
+
         self.ax1.set_xlim(X_MIN - 1, X_MAX + 1)
         self.ax1.set_ylim(Y_MIN - 1, Y_MAX + 1)
         self.ax1.legend()
@@ -172,6 +183,7 @@ class LocPF(PF):
             new_state[0] += noise[0]
             new_state[1] += noise[1]
             new_state[2] += noise[2]
+            new_state[2] = normalize_radians(new_state[2])
             self.last_control_ts = self.current_ts
             return LocPFParticle(
                 new_state[0], new_state[1], new_state[2],
@@ -225,17 +237,34 @@ class LocPF(PF):
         return logweight
 
     def have_more_data(self):
-        return self.current_ts < 200
-        # return self.current_ts < len(self.dataset.timestamps)
+        # return self.current_ts < 500
+        return self.current_ts < len(self.dataset.timestamps)
 
     def hook_after_initialize(self):
         self.old_scatter = None
+        self.old_gps = None
 
     def hook_after_advance(self, new_particles, norm_logweights):
+        self.debug_new_particles = new_particles
         self.debug_logweights = norm_logweights
 
     def hook_after_resample(self, indices):
-        print "{} particles survived".format(len(set(indices)))
+        # dists = [
+        #     (p.x - self.dataset.init_x) ** 2 +
+        #     (p.y - self.dataset.init_y) ** 2
+        #     for p in self.debug_new_particles]
+        # sorted_is = np.argsort(dists)
+        # for i in sorted_is:
+        #     p = self.debug_new_particles[i]
+        #     lw = self.debug_logweights[i]
+        #     print "particle at {}: logweight {} weight {}".format(
+        #         (p.x, p.y, p.theta), lw, np.exp(lw))
+
+        print "-> {} particles survived:".format(len(set(indices)))
+
+        # for i in set(indices):
+        #     p = self.debug_new_particles[i]
+        #     print "particle at {}".format((p.x, p.y, p.theta))
 
     def hook_after_step(self):
         max_logweight = np.max(self.debug_logweights)
@@ -250,21 +279,43 @@ class LocPF(PF):
                 np.exp(max_logweight)))
 
         # Plotting is slow, so don't do it at every time step.
-        # if self.current_ts % 10 == 0:
-        if True:
+        if self.current_ts % 20 == 0:
+        # if True:
             all_particles = []
             for particle in self.particles:
-                all_particles.append((particle.x, particle.y))
+                assert -np.pi <= particle.theta <= np.pi
+                all_particles.append((particle.x, particle.y, particle.theta))
             all_particles = np.array(all_particles)
             if self.old_scatter:
                 self.old_scatter.remove()
             self.old_scatter = self.ax1.scatter(
                 all_particles[:, 0],
                 all_particles[:, 1],
-                s=1,
+                c=all_particles[:, 2],
+                s=10,
+                linewidths=0,
+                vmin=-np.pi,
+                vmax=np.pi,
                 label='all particles')
+
+            best_i = np.argmax(self.debug_logweights)
+            best_p = self.debug_new_particles[best_i]
+            print "best_particle = {}".format(
+                [best_p.x, best_p.y, best_p.theta])
+
+            # Plot the last known ground GPS location.
+            for i in xrange(self.current_ts - 1, -1, -1):
+                if self.dataset.ts2sensor[i] == 'gps':
+                    pose = self.dataset.ground_ts2gps[i]
+                    print "ground_gps = {}".format(pose)
+                    if self.old_gps:
+                        self.old_gps.remove()
+                    self.old_gps = self.ax1.scatter(
+                        [pose[0]], [pose[1]], marker='x')
+                    break
+
             plt.draw()
-            raw_input()
+            # raw_input()
 
 
 # FIXME:
@@ -282,10 +333,26 @@ class LocPF(PF):
 # when the last control reading was processed.
 
 
+def normalize_radians(theta):
+    """
+    Bring angle within [-np.pi, np.pi).
+    """
+    if theta < -np.pi:
+        theta += 2 * np.pi
+    elif theta >= np.pi:
+        theta -= 2 * np.pi
+    assert -np.pi <= theta <= np.pi
+    return theta
+
+
 def demo(dataset_name, num_particles):
     """
     Run LocPF on the given dataset.
     """
+    seed = 666
+    np.random.seed(seed)
+    random.seed(seed)
+
     dataset = Dataset.read(dataset_name)
     pf = LocPF(dataset, num_particles)
     pf.run()
--- python-src/ppaml_car/test_lasers.py
+++ python-src/ppaml_car/test_lasers.py
@@ -1,4 +1,5 @@
 import nose
+import matplotlib.pyplot as plt
 import numpy as np
 
 from ppaml_car import fast
@@ -15,6 +16,36 @@ def test_readings_for_obstacle_vectorized():
     readings_2 = lasers.readings_for_obstacle_vectorized(
         lx, ly, ltheta, laser_angles, laser_max_range, ox, oy, orad)
     nose.tools.assert_true(np.sum(np.abs(readings_1 - readings_2)) < 1e-6)
+
+
+def test_readings_for_obstacles_regression_1():
+    a = 0.299541
+    b = 0.0500507
+    laser_angles = lasers.default_laser_angles()
+    laser_max_range = lasers.default_laser_max_range()
+    obstacles = np.array([
+        [-4.475, 1.45, 0.35],
+        [-1.3, 1.025, 0.35],
+        [-3., -1.55, 0.35],
+        [0.65, -1.95, 0.35],
+        [-1.95, -3.8, 0.35],
+        [0.15, -5.625, 0.35]])
+    x = 0.18361848856646254
+    y = -4.2881577071112806
+    theta = -2.2011317013010205
+    lx, ly, ltheta = lasers.car_loc_to_laser_loc(x, y, theta, a, b)
+    obs_lasers = fast.readings_for_obstacles(
+        lx, ly, ltheta, laser_angles, laser_max_range, obstacles)
+
+    # For debugging when the test fails:
+    if True:
+        lasers.plot_lasers(
+            lx, ly, ltheta, laser_angles, laser_max_range,
+            obstacles, obs_lasers, plt.gca())
+        plt.show()
+
+    # LEFT TODO: currently this is wrong (misses an obstacle)
+    assert False
 
 
 def test_readings_for_obstacles_old_new():
