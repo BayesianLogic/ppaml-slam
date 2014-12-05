875b339dec485459d9a3033462245e72b2626c19
Constantin Berzan <cberzan@gmail.com>
2014-12-04 16:16:29 -0800

    first attempt at optimizing ransac; no improvement

--- python-src/ppaml_car/ransac.py
+++ python-src/ppaml_car/ransac.py
@@ -26,11 +26,7 @@ def extract_obstacles(
     true_laser_std = 0.1
     scoring_laser_std = 2.0
 
-    def calc_score(obstacles):
-        if obstacles:
-            obstacles_arr = np.array(obstacles)
-        else:
-            obstacles_arr = np.empty((0, 3))
+    def calc_score(obstacles_arr):
         lasers = fast.readings_for_obstacles(
             laser_x, laser_y, laser_theta,
             laser_angles, laser_max_range, obstacles_arr)
@@ -41,19 +37,21 @@ def extract_obstacles(
         return dist2 <= known_radius ** 2
 
     # Start with an empty set of obstacles.
-    empty_score = calc_score([])
+    empty_score = calc_score(np.empty((0, 3)))
     # print "empty_score={}".format(empty_score)
 
     # Collect a bunch of obstacles that individually help explain the readings.
     all_obstacles = []
     min_improvement = 2.0
+    tmp_obstacles = np.empty((1, 3))
     for i in xrange(1000):
         index = np.random.randint(len(laser_angles))
         noise = np.random.normal(loc=0.0, scale=true_laser_std)
         x, y = ray_end(
             laser_x, laser_y, laser_theta,
             laser_angles[index], obs_lasers[index] + known_radius + noise)
-        score = calc_score([(x, y, known_radius)])
+        tmp_obstacles[0, :] = x, y, known_radius
+        score = calc_score(tmp_obstacles)
         improvement = score - empty_score
         # print "Considering {}; improvement {}".format((x, y), improvement)
         if improvement >= min_improvement:
@@ -62,29 +60,30 @@ def extract_obstacles(
 
     # Add obstacles greedily from the list of candidates.
     all_candidates = all_obstacles
-    all_obstacles = []
+    all_obstacles = np.empty((0, 3))
     while all_candidates:
         all_score = calc_score(all_obstacles)
         # print "Have {} obstacles; all_score={}".format(
         #     len(all_obstacles), all_score)
-        best_candidate = None
-        best_obstacles = None
+        best_i = None
         best_improvement = -np.inf
-        for candidate in all_candidates:
-            obstacles = all_obstacles + [candidate]
-            score = calc_score(obstacles)
+        tmp_obstacles = np.empty((len(all_obstacles) + 1, 3))
+        tmp_obstacles[:-1, :] = all_obstacles
+        for i in xrange(len(all_candidates)):
+            tmp_obstacles[-1, :] = all_candidates[i]
+            score = calc_score(tmp_obstacles)
             improvement = score - all_score
             if improvement > best_improvement:
-                best_candidate = candidate
-                best_obstacles = obstacles
+                best_i = i
                 best_improvement = improvement
-        assert best_candidate is not None
+        assert best_i is not None
         if best_improvement < min_improvement:
             break
         # print "Added {} for an improvement of {}".format(
         #     candidate, best_improvement)
-        all_obstacles = best_obstacles
+        tmp_obstacles[-1, :] = all_candidates[best_i]
+        all_obstacles = tmp_obstacles
 
     # print "Have {} obstacles; all_score={}".format(
     #     len(all_obstacles), all_score)
-    return np.array(all_obstacles)
+    return all_obstacles
